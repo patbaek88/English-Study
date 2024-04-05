0b766de5923308d88ab2e5f54ebd717d90a71e53
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-05 14:10:32 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[11]:
-
 import streamlit as st
 import pandas as pd
-#import random
 
 # review 데이터 불러오기
 df = pd.read_csv('review.csv')
@@ -13,14 +7,8 @@ df = pd.read_csv('review.csv')
 # 10개의 무작위 샘플 추출
 df_samples = df.sample(n=20)
 
-
-#csv로 저장
-#df_samples.to_csv('quiz.csv', index=False, encoding='cp949')
-
-
 st.title('English Quiz')  # 타이틀명 지정
 st.write("")
-
 st.write(df_samples)
 st.write("")
 
