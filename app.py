fdd3e76427fb1cc7316982170001e611d833cbb5
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 12:53:52 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,13 @@ df_answer = df_samples.loc[:, ['English']]
 st.title('English Quiz')  # 타이틀명 지정
 st.write("")
 st.write(df_quiz)
-if st.button("Answer"):
-  st.success(df_answer)
+#if st.button("Answer"):
+#  st.write(df_answer)
+
+
+if st.checkbox('Answer') #체크 했을때만 정답을 보여줘
+	st.dataframe(df_answer)
+
 if st.button("Reload"):
   st.rerun()
 
