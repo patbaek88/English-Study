1c6da5864d44f5cff2cbc38e0336066b9e4b87bc
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 10:10:22 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
-pd.set_option('display.max.colwidth', 300)
+
+#pd.set_option('display.max.colwidth', 300)
 # review 데이터 불러오기
 df = pd.read_csv('review.csv')
 
 
 # n개의 무작위 샘플 추출
-df_samples = df.sample(n=1)
+df_samples = df.sample(n=10)
 
 st.title('English Quiz')  # 타이틀명 지정
 st.write("")
