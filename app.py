d6beecbe0ec3cacc3b8d48da08a2a6568299c3ed
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 13:07:53 +0900

    Update app.py

--- app.py
+++ app.py
@@ -13,9 +13,9 @@ st.title('English Quiz')  # 타이틀명 지정
 st.write("")
 st.write(df_quiz)
 if st.button("Answer"):
-  st.write(df_answer)
-else:
   st.write("")
+else:
+  st.write(df_answer)
 
 #if st.button("Reload"):
 #  st.write("")
