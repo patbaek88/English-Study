688e3b7958dabd656e99380c9bb7b46951bbaa5a
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 13:13:14 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
 # review 데이터 불러오기
 df = pd.read_csv('review.csv')
 
@@ -12,10 +19,11 @@ df_answer = df_samples.loc[:, ['English']]
 st.title('English Quiz')  # 타이틀명 지정
 st.write("")
 st.write(df_quiz)
+
 if st.button("Answer"):
-  st.write("")
-else:
-  st.write(df_answer)
+  st.write("df_answer")
+
+
 
 #if st.button("Reload"):
 #  st.write("")
