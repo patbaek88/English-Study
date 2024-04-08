cc164d1289be2417d00cf4186f3244eb780361db
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 13:58:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 import streamlit as st
 import pandas as pd
 
+if 'final_dataframe' not in st.session_state:   # session state 에 final 이라는 값이 없으면, 
+   st.session_state['final_dataframe']= df
+   # 초기 값 설정 : session_state에 final_dataframe키 값에 초기값 데이터를 집어넣습니다 .
+
+# #아래 코드는 df의 테이블 값이 바뀌더라도 interactive하게 연동되서 바뀌지 않습니다
+# st.table(df)
+
+# #  아래 코드는  이제 dataframe가 조작될 때 마다 session_state객체 안에 final_dataframe값을 변경하면, 
+# #  수정 될 때  계속 바뀌어서 보여줍니다. 
+# st.table(st.session_state.final_dataframe)
+
 # review 데이터 불러오기
 df = pd.read_csv('review.csv')
 
@@ -13,8 +24,8 @@ st.title('English Quiz')  # 타이틀명 지정
 st.write("")
 #st.write(df_quiz)
 
-#if st.button("Answer"):
-#  st.write(df_answer)
+if st.button("Answer"):
+  st.write(df_answer)
 
 tab1, tab2= st.tabs(['Korean' , 'English'])
 
