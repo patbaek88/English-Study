aed7bb0c8b97bf3b7c46806180b87758b10cc9b8
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-04-08 12:36:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,14 @@ df = pd.read_csv('review.csv')
 
 # n개의 무작위 샘플 추출
 df_samples = df.sample(n=1)
+df_quiz = df_samples.loc[:, ['Korean']]
+df_answer = df_samples.loc[:, ['English']]
 
 st.title('English Quiz')  # 타이틀명 지정
 st.write("")
-#df_quiz = st.dataframe(df_samples, width = 1000)
-st.write(df_samples)
+st.write(df_quiz)
+if st.button("Answer"):
+  st.write(df_answer)
 if st.button("Reload"):
   st.rerun()
 
