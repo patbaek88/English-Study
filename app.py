b2be09a622d01ff747302226cf670b135cb3ef44
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-05-08 15:49:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -18,6 +18,15 @@ if password_input == "cmcpl":
   tts = gTTS(answer, lang='en')
   tts.write_to_fp(sound_file)
 
+  # scenario 데이터 불러오기
+  data = pd.read_csv('scenario.csv')
+  temp_audio_dir = 'temp_audio'
+  os.makedirs(temp_audio_dir, exist_ok=True)
+
+
+
+  
+
   st.title('English Quiz')  # 타이틀명 지정
   st.write("")
   #st.write(df_quiz)
@@ -25,7 +34,7 @@ if password_input == "cmcpl":
   #if st.button("Answer"):
   #  st.write(df_answer)
 
-  tab1, tab2, tab3 = st.tabs(['Korean' , 'English', 'English-Audio'])
+  tab1, tab2, tab3, tab4 = st.tabs(['Korean' , 'English', 'English-Audio', Scenario])
   with tab1:
     #tab A 를 누르면 표시될 내용
     st.table(df_quiz)
@@ -38,6 +47,21 @@ if password_input == "cmcpl":
     #tab C를 누르면 표시될 내용 
     st.audio(sound_file)
 
+  with tab4:
+    #tab 4를 누르면 표시될 내용
+    for index, row in dadta.iterrows():
+      english_sentence = row['English']
+      korean_translation = row['Korean']
+      speaker = row['Name']
+      tts=gTTS(english_sentence, lang='en')
+      audio_file_path = os.path.join(temp_audio_dir, f'audio_{index}.mp3')
+      tts.save(audio_file_path)
+      if st.button(f"재생: {speaker}의 영어문장"):
+        st.audio(audio_file_path)
+
+      st.write(f"번역" {korean_translation}")
+    
+    
   if st.button("Reload"):
     st.write("")
 
