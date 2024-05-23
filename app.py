e090093ed5c2b0a88fb2bd15d55eea637b1d0de8
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-05-24 01:03:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -82,19 +82,8 @@ if password_input == "cmcpl":
 
   st.write("")
   st.write("모든 문장 듣기")
-  #audio_file_all = open('combined_all.mp3', 'rb')
-  #st.audio(audio_file_all.read(), format='audio/mp3')
-  mp3_file = "combined_all.mp3"
-  audio = AudioSegment.from_file(mp3_file, format="mp3")
-
-        # 변환된 WAV 파일을 메모리에 저장
-  wav_io = io.BytesIO()
-  audio.export(wav_io, format="wav")
-  wav_io.seek(0)
-        
-        # 변환된 파일을 재생
-  st.audio(wav_io, format='audio/wav')
-
+  audio_file_all = open('combined_all.mp3', 'rb')
+  st.audio(audio_file_all.read(), format='audio/mp3')
   
   st.write("직장인을 위한 영어패턴1-25 듣기")
   audio_file1 = open('combined1.mp3', 'rb')
