00303c0e2273ef9784ee4f0f2ce1d132db337b91
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-05-28 16:40:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -15,34 +15,40 @@ if password_input == "cmcpl":
 
   # Create Radio Buttons
   topic=st.radio(label = '학습 주제선택', options = ['모두', '직장인을 위한 영어패턴1-25', '미국 직장인이 매일 쓰는 영어 100문장', '원어민이 가장 많이 쓰는 구동사 30개', '일상 영어 회화 패턴 20개', '회사에서 지겹도록 듣게되는 영어 문장 40개'])
+  if st.checkbox('반복재생'):
+    loop = True
+  else:
+    loop = False
+    
   if topic == '모두':
     selected_topics =  ['직장인을 위한 영어패턴1-25', '미국 직장인이 매일 쓰는 영어 100문장', '원어민이 가장 많이 쓰는 구동사 30개', '일상 영어 회화 패턴 20개', '회사에서 지겹도록 듣게되는 영어 문장 40개']
     st.write("모든 문장 듣기")
-    st.audio('combined_all.m4a')
+    st.audio('combined_all.m4a', loop = loop)
   elif topic == '직장인을 위한 영어패턴1-25':
     selected_topics = ['직장인을 위한 영어패턴1-25']
     st.write("직장인을 위한 영어패턴1-25 듣기")
-    st.audio('combined1.m4a')
+    st.audio('combined1.m4a', loop = loop)
   elif topic == '미국 직장인이 매일 쓰는 영어 100문장':
     selected_topics = ['미국 직장인이 매일 쓰는 영어 100문장']
     st.write("미국 직장인이 매일 쓰는 영어 100문장 듣기")
-    st.audio('combined2.m4a')
+    st.audio('combined2.m4a', loop = loop)
   elif topic == '원어민이 가장 많이 쓰는 구동사 30개':
     selected_topics = ['원어민이 가장 많이 쓰는 구동사 30개']
     st.write("원어민이 가장 많이 쓰는 구동사 30개 듣기")
-    st.audio('combined3.m4a')
+    st.audio('combined3.m4a', loop = loop)
   elif topic == '일상 영어 회화 패턴 20개':
     selected_topics = ['일상 영어 회화 패턴 20개']
     st.write("일상 영어 회화 패턴 20개 듣기")
-    st.audio('combined4.m4a')
+    st.audio('combined4.m4a', loop = loop)
   elif topic == '회사에서 지겹도록 듣게되는 영어 문장 40개':
     selected_topics = ['회사에서 지겹도록 듣게되는 영어 문장 40개']
     st.write("회사에서 지겹도록 듣게되는 영어 문장 40개 듣기")
-    st.audio('combined5.m4a')
+    st.audio('combined5.m4a', loop = loop)
   #selected_topics = st.multiselect('학습 주제 선택',  ['직장인을 위한 영어패턴1-25', '미국 직장인이 매일 쓰는 영어 100문장', '원어민이 가장 많이 쓰는 구동사 30개', '일상 영어 회화 패턴 20개', '회사에서 지겹도록 듣게되는 영어 문장 40개'],
   #  default= ['직장인을 위한 영어패턴1-25', '미국 직장인이 매일 쓰는 영어 100문장', '원어민이 가장 많이 쓰는 구동사 30개', '일상 영어 회화 패턴 20개', '회사에서 지겹도록 듣게되는 영어 문장 40개'])
 
-  df = dataframe[dataframe['Topic'].isin(selected_topics)]
+
+    df = dataframe[dataframe['Topic'].isin(selected_topics)]
 
 
 
