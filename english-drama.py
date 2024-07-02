694dc0ccd675d7517c41cc4a722fd02ac4528121
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-07-02 10:27:25 +0900

    Update english-drama.py

--- english-drama.py
+++ english-drama.py
@@ -7,26 +7,16 @@ import os
 password_input = st.text_input("암호를 입력해주세요",type= "password")
 if password_input == "cmcpl":
 
-  st.header("Conference Call Scenario")
+  st.header("English Drama Expressions")
   # scenario 데이터 불러오기
-  filename = st.selectbox('Select a scenario', ('scenario_11_Penmix_financial issue.csv', 'scenario_10_TFS_contamination issue.csv', 'scenario_9_Innovent_CDMO issue.csv', 'scenario_8_TFS_packaging issue.csv', 'scenario_7_TFS_coating issue_2.csv', 'scenario_6_TFS_coating issue_1.csv', 'scenario_5b_Innovent_delivery issue.csv', 'scenario_5_Innovent_scale-up issue.csv', 'scenario_3_Innovent_kick-off_2.csv'))
+  filename = st.selectbox('Select a scenario', ('The Office_S01E01.csv','The Office_S01E02.csv'))
   data = pd.read_csv(filename)
-  data1 = pd.read_csv(filename)
-  data1.set_index('Order', inplace=True)
-  temp_audio_dir = 'temp_audio'
-  os.makedirs(temp_audio_dir, exist_ok=True)
   st.write("")
-  st.subheader("Scenario:")
-  st.write("Participants:")
-  members = data1.groupby("Company")["Name"].unique()  
-  st.write(members)
-
 
   lang_df = pd.DataFrame({'Language':['English', 'Spanish', 'French', 'German', 'Italian', 'Chinese', 'Japanese', 'Korean'],  'Code':['en', 'es', 'fr', 'de', 'it', 'zh', 'ja', 'ko']})
   lang_select = st.selectbox('Select a language', lang_df['Language'])
   lang_code = lang_df[lang_df['Language'] == lang_select]['Code']
   lang = lang_code.iloc[0]
-  #lang = st.selectbox('Select a language', ['en', 'es', 'fr', 'de', 'it', 'zh', 'ja', 'ko'])
   accent = 'com'
   if lang == 'en':
     accent_df = pd.DataFrame({'Accent':['United States', 'United Kingdom', 'Ireland', 'Canada', 'Australia', 'India', 'South Afria'],  'Accent_Code':['com', 'co.uk', 'ie', 'ca', 'com.au', 'co.in', 'co.za']})
@@ -35,23 +25,17 @@ if password_input == "cmcpl":
     accent = accent_code.iloc[0]
 
   
-  sound_file = BytesIO()
-  text = ' '.join(data['English'].astype(str).tolist())
-  tts = gTTS(text, lang=lang, tld=accent)
-  tts.write_to_fp(sound_file)
-  st.write("Play all the sentences")
-  st.audio(sound_file)
+  #sound_file = BytesIO()
   st.write("")
 
   for index, row in data.iterrows():
     english_sentence = row['English']
     korean_translation = row['Korean']
-    speaker = row['Name']
-    order = row['Order']
+    order = row['Number']
     tts=gTTS(english_sentence, lang=lang, tld=accent)
     audio_file_path = os.path.join(temp_audio_dir, f'audio_{index}.mp3')
     tts.save(audio_file_path)
-    st.write(f"{order}. {speaker}")
+    st.write(f"{order}. ")
     st.audio(audio_file_path)
     #if st.button("Show Text"):
     #  st.write(f"{english_sentence}")
