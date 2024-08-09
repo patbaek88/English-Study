cea6ac5396fff9ab6e55f06b3ac7518a09a35979
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-08-09 17:29:46 +0900

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from pydub import AudioSegment
 from streamlit_mic_recorder import mic_recorder
 import speech_recognition as sr
 import tempfile
-#from st_audiorec import st_audiorec
+
 
 password_input = st.text_input("암호를 입력해주세요",type= "password")
 
