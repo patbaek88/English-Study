138632bb6e6b22269850f569ddf6d4e653be4a43
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-08-09 17:14:08 +0900

    Update app.py

--- app.py
+++ app.py
@@ -110,9 +110,8 @@ if password_input == "cmcpl":
     audio_data = mic_recorder()
 
     if audio_data is not None:
-      audio_stream = io.BytesIO(audio_data)
-      with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
-        temp_audio_file.write(audio_stream.read())
+        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_audio_file:
+        temp_audio_file.write(audio_data.getvalue())
         temp_audio_path = temp_audio_file.name    
         
       r = sr.Recognizer()
