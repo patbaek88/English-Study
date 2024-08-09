689e1aabea39a5dbd8f7b9d78c573a480e7f49ea
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2024-08-09 16:49:02 +0900

    Update app.py

--- app.py
+++ app.py
@@ -127,8 +127,7 @@ if password_input == "cmcpl":
           st.write("Sorry, I could not understand the audio.")        
         except sr.RequestError as e:
           st.write("Could not request results from Google Web Speech API; {0}".format(e))
-    
-       os.remove(temp_audio_path)
+      os.remove(temp_audio_path)
   
   if st.button("Reload"):
     st.write("")
