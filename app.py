28a960ec2208d2c6d50a1b127ac0c2f3e7159da9
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-01-09 13:02:23 +0900

    Update app.py

--- app.py
+++ app.py
@@ -86,7 +86,7 @@ if password_input == "cmcpl":
   # n개의 무작위 샘플 추출
   #n_quiz = st.number_input('한번에 나오는 문제 수 설정', 0, 99, value = 1)
   n_quiz =1
-  df_samples = df.sample(n=n_quiz)
+  df_samples = df.sample(n=n_quiz, replace=False)
   df_quiz = df_samples.loc[:, ['Korean']]
   df_answer = df_samples.loc[:, ['English']]
   quiz = df_quiz.iloc[0,0]
