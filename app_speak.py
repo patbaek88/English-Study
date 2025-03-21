aaa233eab1fef4b34f67dfb0c64d77753e1d1bc5
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-22 01:30:40 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -33,10 +33,8 @@ if password_input == "cmcpl":
   df = dataframe[dataframe["Topic"].isin(selected_topics)]
 
 
-  if st.checkbox('반복재생'):
-    loop = True   
-  else:
-    loop = False
+  # 반복 재생 여부 체크박스 추가
+  repeat_audio = st.checkbox("반복 재생")
     
   # 음성 파일을 저장할 메모리 버퍼 생성
   audio_bytes = io.BytesIO()
@@ -53,7 +51,7 @@ if password_input == "cmcpl":
       tts_en.write_to_fp(combined_audio)
 
   # Streamlit에서 오디오 재생
-  st.audio(combined_audio.getvalue(), format="audio/mp3")
+  st.audio(combined_audio.getvalue(), format="audio/mp3", loop=repeat_audio)
   
   
   with st.expander('선택한 학습 주제의 모든 문장 보기'):
