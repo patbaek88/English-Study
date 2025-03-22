391ae4c7d0d31e82cc3ba04df2799f8887b6e945
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-23 03:06:04 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -119,21 +119,19 @@ if password_input == "cmcpl":
       
       st.audio(sound_file, autoplay=autoplay)
 
+  # microphone에서 auido source를 생성합니다
   r = sr.Recognizer()
-
   with sr.Microphone() as source:
+      st.write("Say something!")
       audio = r.listen(source)
-
+  
   # 구글 웹 음성 API로 인식하기 
   try:
-      print("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
-      return r.recognize_google(audio, language='ko')
+      st.write("Google Speech Recognition thinks you said : " + r.recognize_google(audio, language='ko'))
   except sr.UnknownValueError as e:
-      print("Google Speech Recognition could not understand audio".format(e))
-      return None
+      st.write("Google Speech Recognition could not understand audio".format(e))
   except sr.RequestError as e:
-      print("Could not request results from Google Speech Recognition service; {0}".format(e))
-      return None
+      st.write("Could not request results from Google Speech Recognition service; {0}".format(e))
     
       
   if st.button("Reload"):
