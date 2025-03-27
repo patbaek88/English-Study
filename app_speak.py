c4cfcf90111e18898c0290c3c7549bbb05a694cd
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-27 15:36:10 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -20,7 +20,7 @@ if password_input == "cmcpl":
 
   
   # review 데이터 불러오기
-  dataframe = pd.read_csv('review_speak.csv', encoding="euc-kr")
+  dataframe = pd.read_csv('review_speak.csv') #encoding="euc-kr")
 
   topics = dataframe["Topic"].drop_duplicates().tolist()
 
