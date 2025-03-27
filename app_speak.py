6aa8991cdfade37fbc13927a54b102f90a077ff9
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-27 17:43:54 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -117,20 +117,29 @@ if password_input == "cmcpl":
       #tab 4 를 누르면 표시될 내용
       st.table(df_quiz)
 
-      #if "audio_data1" not in st.session_state:
-      #  st.session_state.audio_data1 = None
+      if "audio_data1" not in st.session_state:
+        st.session_state.audio_data1 = None
+
+      if "recoding_done" not in st.session_state:
+        st.session_state.recording_done = False
+
+      if not st.session_sate.recoring_done: 
+        st.session_state.audio_data1 = st.audio_input("Record English sentences")
+        if st.session_state.audio_data1:
+          st.session_state.recording_done = True
+          st.experimental_rerun()
+
 
       #st.session_state.audio_data1 = st.audio_input("Record English sentences")
-      audio_data1 = st.audio_input("Record English sentences")
+      #audio_data1 = st.audio_input("Record English sentences")
 
+      #if audio_data1 is not None:
+      #  audio_bytes1 = io.BytesIO(audio_data1.read())
+        #if audio_data1.type == "audio/mpeg":
 
-     
-      #if st.session_state.audio_data1 is not None:
-      #  audio_bytes1 = io.BytesIO(st.session_state.audio_data1.read())
-      #  if st.session_state.audio_data1.type == "audio/mpeg":
-      if audio_data1 is not None:
-        audio_bytes1 = io.BytesIO(audio_data1.read())
-        if audio_data1.type == "audio/mpeg":
+      if st.session_state.audio_data1 is not None:
+        audio_bytes1 = io.BytesIO(st.session_state.audio_data1.read())
+        if st.session_state.audio_data1.type == "audio/mpeg":
           audio1 = AudioSegment.from_mp3(audio_bytes1)
           audio_bytes1 = io.BytesIO()
           audio1.export(audio_bytes1, format ="wav")
@@ -141,7 +150,7 @@ if password_input == "cmcpl":
 
         try:
           text1 = recognizer1.recognize_google(audio1, language = "en")
-          st.write(f"{text1}")
+          st.write(f"인식된 문장: {text1}")
           st.table(df_answer)
         except sr.UnknownValueError:
           st.write("음성을 인식할 수 없습니다.")
@@ -150,7 +159,8 @@ if password_input == "cmcpl":
    
       
   if st.button("Reload"):
-    #audio_data1 = None
+    st.session_state.audio_data1 = None
+    st.session_state.recording_done = False
     st.write("")
 
 
