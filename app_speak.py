6b3a3bb672f17c368d75304b365484b3c5f9e482
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-29 03:53:27 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -137,7 +137,8 @@ if password_input == "cmcpl":
     # 유사도 반환 (0과 1 사이 값, 1에 가까울수록 유사)
     return cosine_sim[0][0]
 
-  audio_data1 = st.audio_input(f"Record: {saved_answer}")
+  a = answer
+  audio_data1 = st.audio_input(f"Record: {a}")
 
   if audio_data1 is not None:
     audio_bytes1 = io.BytesIO(audio_data1.read())
