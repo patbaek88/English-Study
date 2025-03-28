5e68ec2bccccaa078c91a1bf06b52c37173e497d
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-29 00:35:37 +0900

    Update app_speak.py

--- app_speak.py
+++ app_speak.py
@@ -126,7 +126,7 @@ if password_input == "cmcpl":
           audio_bytes1 = io.BytesIO()
           audio1.export(audio_bytes1, format ="wav")
 
-        st.audio(audio_bytes, start_time=0)
+        st.audio(audio_bytes1, start_time=0)
 
         recognizer1 = sr.Recognizer()
         with sr.AudioFile(audio_bytes1) as source:
