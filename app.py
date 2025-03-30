9795120403c3c51ff45a1a188b85cb92c422d576
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-03-30 23:54:54 +0900

    Update app.py

--- app.py
+++ app.py
@@ -148,8 +148,8 @@ if password_input == "cmcpl":
 
 
   
-  
-  audio_data1 = st.audio_input("녹음할 문장: "+answer)
+  st.write("녹음할 문장: "+answer)
+  audio_data1 = st.audio_input("")
 
   if audio_data1 is not None:
     audio_bytes1 = io.BytesIO(audio_data1.read())
