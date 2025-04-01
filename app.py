92630aca45021509ad60859a15e4a729bca63a30
patbaek88 <141615314+patbaek88@users.noreply.github.com>
2025-04-01 12:44:31 +0900

    Update app.py

--- app.py
+++ app.py
@@ -90,6 +90,7 @@ if password_input == "cmcpl":
   if remaining_samples.empty:
     st.write("No more new quizzes available!")
     st.session_state.used_samples = []
+    df_answer = []
     
   else:
     df_samples = remaining_samples.sample(n=1, replace=False)
